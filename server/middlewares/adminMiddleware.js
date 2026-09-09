886891b3321cac2422bfb051639b306dc7b52d40
Bharath Reddy <bharathmadatala2@gmail.com>
2026-09-09 23:49:54 +0530

    Optimize: Check isAdmin from JWT token instead of DB lookup on every request

--- server/middlewares/adminMiddleware.js
+++ server/middlewares/adminMiddleware.js
@@ -1,14 +1,13 @@
 const jwt = require("jsonwebtoken");
-const User = require("../models/userModel");
 
-module.exports = async (req, res, next) => {
+module.exports = (req, res, next) => {
   try {
     const token = req.headers.authorization.split(" ")[1];
     const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
     const userId = decodedToken.userId;
+    const isAdmin = decodedToken.isAdmin;
 
-    const user = await User.findById(userId);
-    if (!user || !user.isAdmin) {
+    if (!isAdmin) {
       return res.status(403).send({
         message: "You are not authorized",
         success: false,
